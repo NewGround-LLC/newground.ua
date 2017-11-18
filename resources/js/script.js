c32d647db55fba9f567e00ae8af89df4d4c92e1a
isMaxVelychko <ismaxvelychko@gmail.com>
2017-11-18 22:17:25 +0200

    Do not use scroll plugin

--- resources/js/script.js
+++ resources/js/script.js
@@ -13,7 +13,7 @@ function handlePageLoaded() {
         $('#status').fadeOut();
         $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
         $('body').delay(350).css({'overflow':'visible'});
-        setScrollBehavior();
+        // setScrollBehavior();
     });
 }
 
