bc82812e67ad3c3a09b249e071798dd351f9de1f
Iaroslav Omelianenko <yaric_mail@yahoo.com>
2018-01-31 18:32:45 +0200

    Fixed JSLint errors

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,37 +1,33 @@
-"use strict";
-
-init();
-
-
-function init() {
-    handlePageLoaded();
-    handleLinkClick();
-}
+/*jslint browser: true*/
+/*global $, window, location */
 
 function handlePageLoaded() {
-    $(window).on("load", () => {
+    'use strict';
+    $(window).on("load", function () {
         $('#status').fadeOut();
         $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-        $('body').delay(350).css({'overflow':'visible'});
+        $('body').delay(350).css({'overflow': 'visible'});
         // setScrollBehavior();
     });
 }
 
-function setScrollBehavior() {
+/*function setScrollBehavior() {
+    'use strict';
     $("html").niceScroll({
-        cursorcolor:"#f74d65",
-        scrollspeed :"100",
-        cursorborder:"1px solid #f74d65",
+        cursorcolor: "#f74d65",
+        scrollspeed: "100",
+        cursorborder: "1px solid #f74d65",
         horizrailenabled: "false",
         cursorborderradius: "0px"
     });
-}
+}*/
 
 function handleLinkClick() {
-    $('a[href*="#"]:not([href="#"])').on("click", () => {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+    'use strict';
+    $('a[href*="#"]:not([href="#"])').on("click", function () {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
             var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
                     scrollTop: target.offset().top
@@ -41,3 +37,11 @@ function handleLinkClick() {
         }
     });
 }
+
+function init() {
+    'use strict';
+    handlePageLoaded();
+    handleLinkClick();
+}
+
+init();
